3b50185f026a3d2f733dadf3969b0d222af195d4
Itamar <pitdav@gmail.com>
2018-05-28 11:25:27 +0300

    Parrell CS

--- CS_project/CSMRI/Run_CS.py
+++ CS_project/CSMRI/Run_CS.py
@@ -5,10 +5,53 @@ import scipy.io as imp
 import os
 from CS_ops import *
 from CS import CS
+from threading import Thread,BoundedSemaphore
+
+
+def CS_thread(case,mask_name):
+    threadLimiter.acquire()
+    mask = np.array(masks[mask_name])
+    nii_path = DATA_DIR + 'input/' + case + '/' + case + NII_SUFFIX
+    rec_dir = DATA_DIR + 'rec_images/' + case
+
+    CS_path = os.path.join(rec_dir, case + '_CS_' + mask_name + NII_SUFFIX)
+    ZF_path = os.path.join(rec_dir, case + '_ZF_' + mask_name + NII_SUFFIX)
+
+    if not os.path.exists(rec_dir):
+        os.makedirs(rec_dir)
+    print('Working on : {0} mask: {1}'.format(nii_path, mask_name))
+
+    nii = nib.load(nii_path)
+    data = nii.get_data()
+    # ZF_data = np.zeros_like(data).astype(np.float32)
+    CS_data = np.zeros_like(data).astype(np.float32)
+
+    max_data_val = np.max(abs(data))
+    data = data.astype(np.float32) / max_data_val
+
+    for i in range(data.shape[2]):
+
+        kspace = fft2c(data[:, :, i])
+        masked_kspace = kspace * mask
+        CS_rec = CS(kspace, mask, params)
+        CS_data[:, :, i] = abs(ifft2c(CS_rec))
+        if i % 20 == 0:
+            print('Slice : {0}'.format(i))
+
+    # ZF_nii = nib.Nifti1Image(ZF_data, nii.affine)
+    CS_nii = nib.Nifti1Image(CS_data, nii.affine)
+    # ZF_nii.update_header()
+    CS_nii.update_header()
+
+    # nib.save(ZF_nii, ZF_path)
+    nib.save(CS_nii, CS_path)
+    threadLimiter.release()
+
+threadLimiter = BoundedSemaphore(10)
 
 NII_SUFFIX = '.nii.gz'
 DATA_DIR = 'data/'
-masks_names = ['mask10','mask33','mask50','mask66','mask_33_row','mask_50_row','mask_66_row']
+masks_names = ['mask50_1']#'mask50r_1']#'mask_4_1']#'mask_4_1'] #]#, 'mask_4_1']  #,'mask66','mask_33_row','mask_50_row','mask_66_row']
 
 params = {}
 params['thresh'] = 0.0075
@@ -23,38 +66,6 @@ sub_dirs = os.listdir(DATA_DIR+'input')
 
 for case in sub_dirs:
     for mask_name in masks_names:
-        mask = np.array(masks[mask_name])
-        nii_path = DATA_DIR+'input/'+case+'/'+case+NII_SUFFIX
-        rec_dir = DATA_DIR+'rec_images/'+case
-
-        CS_path = os.path.join(rec_dir, case + '_CS_'+mask_name + NII_SUFFIX)
-        ZF_path = os.path.join(rec_dir, case + '_ZF_'+mask_name + NII_SUFFIX)
-
-        if not os.path.exists(rec_dir):
-            os.makedirs(rec_dir)
-        print('Working on : {0} mask: {1}'.format(nii_path,mask_name))
-
-        nii = nib.load(nii_path)
-        data = nii.get_data()
-        ZF_data = np.zeros_like(data).astype(np.float32)
-        CS_data = np.zeros_like(data).astype(np.float32)
-
-        max_data_val = np.max(abs(data))
-        data = data.astype(np.float32) / max_data_val
-
-        for i in xrange(data.shape[2]):
-            kspace = fft2c(data[:, :, i])
-            masked_kspace = kspace * mask
-            ZF_data[:, :, i] = abs(ifft2c(masked_kspace))
-            CS_rec = CS(kspace, mask, params)
-            CS_data[:, :, i] = abs(ifft2c(CS_rec))
-            if i%20==0:
-                print('Slice : {0}'.format(i))
-
-        ZF_nii = nib.Nifti1Image(ZF_data, nii.affine)
-        CS_nii = nib.Nifti1Image(CS_data, nii.affine)
-        ZF_nii.update_header()
-        CS_nii.update_header()
-
-        nib.save(ZF_nii, ZF_path)
-        nib.save(CS_nii, CS_path)
+        t =Thread(target=CS_thread,args=(case,mask_name))
+        t.start()
+t.join()
